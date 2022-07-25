b7bafab421482473d8ec32587020db43a6c6c0b3
джо <stas.titovich@gmail.com>
2022-07-25 23:21:54 +0300

    application failed я хз 3

--- src/main/java/com/example/swit/domain/Message.java
+++ src/main/java/com/example/swit/domain/Message.java
@@ -1,8 +1,9 @@
 package com.example.swit.domain;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Entity
 public class Message {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
