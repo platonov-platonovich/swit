85a3e976ecca169ae063bd14c243629d3a9924bd
джо <stas.titovich@gmail.com>
2022-07-25 23:20:42 +0300

    application failed я хз 2

--- src/main/java/com/example/swit/domain/Message.java
+++ src/main/java/com/example/swit/domain/Message.java
@@ -10,7 +10,12 @@ public class Message {
     private String text;
     private String tag;
 
+    public Message() {
+    }
+
     public Message(String text, String tag) {
+        this.text=text;
+        this.tag=tag;
     }
 
     public String getText() {
